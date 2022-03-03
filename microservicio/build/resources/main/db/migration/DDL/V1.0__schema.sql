4a1107e1d1b05431460d1edc10ae956052b7f586
Rodrigo Esteban Beltran Abello - Ceiba Software <esteban.beltran@ceiba.com.co>
2022-03-03 11:30:00 -0500

    Quite toda referencia a usuario

--- microservicio/build/resources/main/db/migration/DDL/V1.0__schema.sql
+++ microservicio/build/resources/main/db/migration/DDL/V1.0__schema.sql
@@ -1,11 +1,3 @@
-create table usuario (
- id int(11) not null auto_increment,
- nombre varchar(100) not null,
- clave varchar(45) not null,
- fecha_creacion datetime null,
- primary key (id)
-);
-
 CREATE TABLE jugador (
  id INT NOT NULL AUTO_INCREMENT,
  documento INT NOT NULL unique,
--- microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
+++ microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
@@ -1,5 +1,3 @@
-insert into usuario(id, nombre,clave,fecha_creacion) values(1,'test','1234',now());
-
 insert into jugador (documento, nombre, apellido, fecha_nacimiento, peso, altura, posicion, pie_habil)
 values (80808080, 'Juanito', 'Perez', now(), 45.6, 1.65, 'Portero', 'Derecho');
 
