afb9895a26254ada70b817a487cb5889b0a220a6
Rodrigo Esteban Beltran Abello - Ceiba Software <esteban.beltran@ceiba.com.co>
2022-03-10 14:57:09 -0500

    Se creo la asistencia

--- microservicio/dominio/src/test/java/com/ceiba/factura/servicio/ServicioEliminarFacturaTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/factura/servicio/ServicioEliminarFacturaTest.java
@@ -1,9 +1,23 @@
 package com.ceiba.factura.servicio;
 
+import com.ceiba.asistencia.modelo.entidad.Asistencia;
+import com.ceiba.asistencia.puerto.repositorio.RepositorioAsistencia;
+import com.ceiba.asistencia.servicio.ServicioCrearAsistencia;
+import com.ceiba.asistencia.servicio.testdatabuilder.AsistenciaTestDataBuilder;
+import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
+import com.ceiba.factura.modelo.entidad.Factura;
 import com.ceiba.factura.puerto.repositorio.RepositorioFactura;
+import com.ceiba.factura.servicio.testdatabuilder.FacturaTestDataBuilder;
+import com.ceiba.jugador.modelo.entidad.Jugador;
+import com.ceiba.jugador.servicio.testdatabuilder.JugadorTestDataBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
 
 public class ServicioEliminarFacturaTest {
 
--- microservicio/dominio/src/test/java/com/ceiba/jugador/servicio/ServicioEliminarJugadorTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/jugador/servicio/ServicioEliminarJugadorTest.java
@@ -1,9 +1,19 @@
 package com.ceiba.jugador.servicio;
 
+import com.ceiba.BasePrueba;
+import com.ceiba.asistencia.modelo.entidad.Asistencia;
+import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
+import com.ceiba.jugador.modelo.entidad.Jugador;
 import com.ceiba.jugador.puerto.repositorio.RepositorioJugador;
+import com.ceiba.jugador.servicio.testdatabuilder.JugadorTestDataBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
 
 public class ServicioEliminarJugadorTest {
 
@@ -17,6 +27,23 @@ public class ServicioEliminarJugadorTest {
 
         Mockito.verify(repositorioJugador, Mockito.times(1)).eliminar(1l);
 
+    }
+
+    @Test
+    @DisplayName("Deberia lanzar una excepcion si esta registrado en otra tabla")
+    void deberiaLanzarUnaExcepcionSiEstaRegistradoEnOtraTabla(){
+        Jugador jugador = new JugadorTestDataBuilder().conId(1L).build();
+        RepositorioJugador repositorioJugador = Mockito.mock(RepositorioJugador.class);
+
+        doAnswer(invocation -> {
+            Jugador jugadorArg = invocation.getArgument(0);
+            assertNotNull(jugadorArg);
+            return null;
+        }).when(repositorioJugador).crear(any(Jugador.class));
+        Mockito.when(repositorioJugador.existeJugadorConFactura(Mockito.anyLong())).thenReturn(true);
+        ServicioEliminarJugador servicioEliminarJugador = new ServicioEliminarJugador(repositorioJugador);
+        // act - assert
+        assertThrows(ExcepcionDuplicidad.class, () -> servicioEliminarJugador.ejecutar(jugador.getId()));
     }
 
 }
